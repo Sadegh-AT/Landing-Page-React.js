f2fed3e8c377161bb58281947029f76f7320d968
SadeghAT <sadegh.artivox@gmail.com>
2023-03-25 00:59:28 +0430

    move boxes from header to main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // Header
 import Header from "./Components/Header/Header";
 
+// Main
+import Main from "./Components/Main/Main";
+
 import "./App.css";
 function App() {
   return (
     <div>
       <Header></Header>
+      <Main></Main>
     </div>
   );
 }
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Main.css";
+
+//Boxrs
+import Boxes from "./Boxes/Boxes";
+
+export default function Main() {
+  return (
+    <div>
+      <div className="container">
+        <Boxes></Boxes>
+      </div>
+    </div>
+  );
+}
