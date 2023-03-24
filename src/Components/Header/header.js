7d8e3a187b1c0b321d09189532ebf1b128359a9d
SadeghAT <sadegh.artivox@gmail.com>
2023-03-24 23:08:48 +0430

    header content

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -9,6 +9,14 @@ export default function Header() {
     <div className="header">
       <div className="container">
         <Navbar></Navbar>
+        <div className="haeder-content">
+          <h1 className="header-title">Find Yourself Outside.</h1>
+          <h4 className="header-text">
+            Book unique camping experiences on over 300,000 campsites, cabins,
+            RV parks, public parks and more.
+          </h4>
+          <button className="header-btn">Discover</button>
+        </div>
       </div>
     </div>
   );
